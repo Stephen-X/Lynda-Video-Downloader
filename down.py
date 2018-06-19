7d385ca5003b4f30c9a8645d98a1f452e09c0725
Stephen Tse <Stephen-X@users.noreply.github.com>
2018-06-18 23:54:00 -0400

    Correct comment in code

--- down.py
+++ down.py
@@ -34,8 +34,8 @@ DOWN_SUB = True
 ydl_opts = {
     # Download the best quality available in Lynda (i.e. 720p)
 	# The following means "download `bestvideo` and `bestaudio` separately and mux them together
-	# into a single file giving the best overall quality available; if ffmpeg / avconv is not
-	# installed, fall back to `best` and download the best available quality served as a single file".
+	# into a single file giving the best overall quality available; if not available, fall back
+	# to `best` and download the best available quality served as a single file".
     # More options: https://github.com/rg3/youtube-dl/blob/master/README.md#format-selection
     'format': 'bestvideo+bestaudio/best',
 
