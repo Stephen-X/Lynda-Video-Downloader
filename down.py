9b015ea53e2b6397b376f118afc3e5f11a7f1e0f
Stephen Tse <Stephen-X@users.noreply.github.com>
2018-06-29 14:19:04 -0400

    Some doc improvements

--- down.py
+++ down.py
@@ -6,7 +6,7 @@ course videos and subtitles from Lynda.com.
 Ref: https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
 """
 
-from __future__ import print_function, unicode_literals  # for backward compatibility
+from __future__ import print_function, unicode_literals
 import youtube_dl
 
 # -- Set up parameters --------------------------------------------
@@ -32,30 +32,33 @@ DOWN_SUB = True
 # 'Available options') in ydl's source code:
 # https://github.com/rg3/youtube-dl/blob/master/youtube_dl/YoutubeDL.py
 ydl_opts = {
-
     # -- For video downloads ----------------------------------------------
-
-    # Download the best quality available
     # More options: https://github.com/rg3/youtube-dl/blob/master/README.md#format-selection
 
-    # The following means "download `bestvideo` and `bestaudio` separately and mux them together
-    # into a single file giving the best overall quality available; if not available, fall back
-    # to `best` and download the best available quality served as a single file".
+    # The following means "download `bestvideo` and `bestaudio` separately and
+    # mux them together into a single file giving the best overall quality
+    # available; if not available, fall back to `best` and download the best
+    # available quality served as a single file".
+
     'format': 'bestvideo+bestaudio/best',
 
-    # The following simply download the best available quality served as a single file. Note
-    # that this may not download the best available quality in websites such as YouTube, since
-    # they separate video and audio tracks for videos with resolution higher than 720p.
+    # The following simply download the best available quality served as a
+    # single file. Note that this may not download the best available quality
+    # in websites such as YouTube, since they separate video and audio tracks
+    # for videos with resolution higher than 720p.
+
     # 'format': 'best',
 
     # -- For audio downloads ---------------------------------------------
 
-    # The following will first download audio track with the best possible quality, then convert
-    # to mp3 with a bit rate of 192 kbps.
-    # Look here for a complete list of available postprocessors:
+    # The following will first download audio track with the best possible
+    # quality, then convert to mp3 with a bit rate of 192 kbps.
+    # Look here for a complete list of available postprocessors (remember to
+    # first remove the last 'PP' from the postprocessor names):
     # https://github.com/rg3/youtube-dl/blob/master/youtube_dl/postprocessor/__init__.py
-    # and the constructor of the selected postprocessor for keyword names (remember to
-    # remove the last 'PP' from the postprocessor names).
+    # and check out the constructor of the source code of selected
+    # postprocessors for available options.
+
     # 'format': 'bestaudio/best',
     # 'postprocessors': [{
     #     'key': 'FFmpegExtractAudio',
