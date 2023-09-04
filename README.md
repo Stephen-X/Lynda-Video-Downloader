86ad374afaf5c2b05a03f58621c52d3bb07965a8
Stephen Tse <Stephen-X@users.noreply.github.com>
2023-09-04 04:55:01 -0700

    Switch to yt-dlp

--- README.md
+++ README.md
@@ -7,24 +7,24 @@ A convenient script for downloading and organizing [Lynda.com](https://www.lynda
 
 ## Usage
 
-1. Make sure you have Python (either 2 or 3) installed and accessible via command line, then install [youtube-dl](http://rg3.github.io/youtube-dl/) by `pip install youtube-dl`. If you have both Python 2 and 3 installed in your system, check that you're using the correct `pip` alias for the target python version (e.g. it could be `pip3` for Python 3).
+1. Make sure you have Python 3 installed and accessible via command line, then install [yt-dlp](https://github.com/yt-dlp/yt-dlp/wiki/Installation) by `pip install yt-dlp`. If you have both Python 2 and 3 installed in your system, check that you're using the correct `pip` alias for the target python version (e.g. it could be `pip3` for Python 3).
 
-2. Install [FFmpeg](https://www.ffmpeg.org/) or [avconv](https://libav.org/avconv.html), and make sure it's accessible via command line. Either of them is required to mux video and audio into one single file when downloading from sites like YouTube (they keep videos and audios with *best possible* qualities to their separate files). I recommend that you use a package manager to install either of them. On Windows, you may install [Scoop](https://scoop.sh/) then `scoop install ffmpeg`; on MacOS, you may install [Homebrew](https://brew.sh/) then `brew install ffmpeg` for FFmpeg ([More options](https://trac.ffmpeg.org/wiki/CompilationGuide/macOS#ffmpegthroughHomebrew)) or `brew install libav` for avconv.
+2. Install [FFmpeg](https://www.ffmpeg.org/), and make sure it's accessible via command line. It is required to mux video and audio into one single file when downloading from sites like YouTube (they keep videos and audios with *best possible* qualities to their separate files). I recommend that you use a package manager to install it. On Windows, you may install [Scoop](https://scoop.sh/) then `scoop install ffmpeg`; on MacOS, you may install [Homebrew](https://brew.sh/) then `brew install ffmpeg` for FFmpeg ([More options](https://trac.ffmpeg.org/wiki/CompilationGuide/macOS#ffmpegthroughHomebrew)).
 
-2. Set up your credentials. There're two scenarios:
+3. (Optional) Set up your credentials. There're two scenarios:
     1) You have your own Lynda.com account:
 
-        Set line 16 of `down.py` to `USE_COOKIES = False`, then fill in your username and password in line 20 and 21.
+        Set line 28 of `down.py` to `USE_COOKIES = False`, then fill in your username and password in line 37 and 38.
 
     2) Your Lynda.com account is associated with your organization account, i.e. you sign in with your organization portal:
 
-        2.1. Install the [cookies.txt](https://chrome.google.com/webstore/detail/cookiestxt/njabckikapfpffapmjgojcnbfjonfjfg) Chrome extension, log in to [Lynda.com](https://www.lynda.com/) with your organization portal, then dump the cookies of the Lynda site using the extension (click the extension icon, then click "To download cookies for this tab click here" in the pop-up window). Save your cookies as `cookies.txt` and place it in the same directory as `down.py`.
+        2.1. Log into your account using your preferred browser. Supported browsers are listed in the documentation for [`--cookies-from-browser`](https://github.com/yt-dlp/yt-dlp#filesystem-options).
 
-        2.2. Set line 16 of `down.py` to `USE_COOKIES = True`.
+        2.2. Set line 28 of `down.py` to `USE_COOKIES = True`. Modify line 35 to instruct yt-dlp to load cookies from the target browser.
 
-3. Put URLs of all courses that you wish to download inside a file named `urls.txt` and place it in the same directory as `down.py`. Here I included a sample `urls.txt` file (FYI the included courses are from the famous Adobe Photoshop & Illustrator One-on-One series by [Deke McClelland](https://www.deke.com/)), but you should remember to clear the file content first if you'd like to download something else.
+4. Put URLs of all courses that you wish to download inside a file named `urls.txt` and place it in the same directory as `down.py`. Here I included a sample `urls.txt` file (FYI the included courses are from the famous Adobe Photoshop & Illustrator One-on-One series by [Deke McClelland](https://www.deke.com/)), but you should remember to clear the file content first if you'd like to download something else.
 
-4. Run the script by `python down.py` (similar to stated previously, check your `python` alias if you have both Python 2 and 3 installed on your system). It will download specified courses to the same directory as `down.py`. Here's a sample download outcome after running the script with the sample URLs:
+5. Run the script by `./down.py`. It will download specified courses to the same directory as `down.py`. Here's a sample download outcome after running the script with the sample URLs:
 
 ```
 Lynda.com- Photoshop CC 2018 One-on-One - Fundamentals
@@ -60,5 +60,7 @@ FYI for Windows users, if you're looking for an all-purpose media player that ca
 
 
 ## Notes
+
+**2023-09-04**: This was originally written for `youtube-dl`. The script has since been updated to use the more powerful folk [`yt-dlp`](https://github.com/yt-dlp/yt-dlp). 
 
 This is just a wrapper program of the wonderful [youtube-dl](http://rg3.github.io/youtube-dl/), a powerful command-line tool that allows you to conveniently download videos, audios, playlists and episodes from [many websites](http://rg3.github.io/youtube-dl/supportedsites.html). My intention of creating this script is to provide a simple cross-platform wrapper script that can quickly download multiple courses / playlists without going through the entire lengthy [documentation](https://github.com/rg3/youtube-dl/blob/master/README.md) (I got you covered there :wink:). __In fact, with minor or no tweaking, this script can also be reused to download contents from other websites such as courses on Udemy, TV episodes on YouTube or music collections on SoundCloud!__ All credits go to the [contributors](http://rg3.github.io/youtube-dl/about) behind `youtube-dl`! :heart_eyes:
--- down.py
+++ down.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 """
-A convenient wrapper program of youtube-dl for downloading and organizing
+A convenient wrapper program of yt-dlp (youtube-dl fork) for downloading and organizing
 course videos and subtitles from Lynda.com.
 
-Ref: https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
+Ref: https://github.com/yt-dlp/yt-dlp#embedding-yt-dlp
 """
-
-# I heard that most Mac people still primarily use built-in Python 2...
-from __future__ import print_function, unicode_literals
 
 # I heard that people don't upgrade their Python packages often...
 import sys
 import subprocess
 # Call pip through the current Python executable to avoid installing packages
 # to the wrong Python distribution
-subprocess.call([sys.executable, '-m', 'pip', 'install', '-U', 'youtube-dl'])
+subprocess.call([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp'])
+# subprocess.call(['mamba', 'update', 'yt-dlp'])
 
-import youtube_dl
+from yt_dlp import YoutubeDL
 
 # -- Set up parameters --------------------------------------------
 
 # If your Lynda account uses organization login, then you should supply cookies
-# instead of username and password to youtube-dl
-USE_COOKIES = True
-# Location to your cookies file
-COOKIEFILE = 'cookies.txt'
+# instead of username and password to yt-dlp.
+# Set this to `True` to use cookies to authenticate, `False` to use username+password,
+# `None` to disable login.
+USE_COOKIES = None
+# Name of the browser to load cookies from. Supported browsers:
+# https://github.com/yt-dlp/yt-dlp#filesystem-options
+# Format: A tuple containing (1) the name of the browser, (2) the profile name/path
+# from where cookies are loaded, (3) the name of the keyring, and
+# (4) the container name, e.g. ('chrome', ) or ('vivaldi', 'default', 'BASICTEXT')
+# or ('firefox', 'default', None, 'Meta')
+COOKIES_CONFIG = ('brave', )
 # Set the following two up if USE_COOKIES is False
 USERNAME = 'your_username'
 PASSWORD = 'your_password'
@@ -39,21 +44,19 @@ DOWN_SUB = True
 # Parameters passed to youtube-dl
 # For complete list of available options, check out the comments (search for
 # 'Available options') in ydl's source code:
-# https://github.com/rg3/youtube-dl/blob/master/youtube_dl/YoutubeDL.py
+# https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py
 ydl_opts = {
     # -- For video downloads ----------------------------------------------
-    # More options: https://github.com/rg3/youtube-dl/blob/master/README.md#format-selection
+    # More options: https://github.com/yt-dlp/yt-dlp#format-selection
 
     # The following means "download `bestvideo` and `bestaudio` separately and
     # mux them together into a single file giving the best overall quality
     # available; if not available, fall back to `best` and download the best
     # available quality served as a single file".
 
-    'format': 'bestvideo+bestaudio/best',
-    # Use MKV as container format when merging best video and audio
-    # tracks. Note that MP4 format is not recommended because it supports
-    # only limited formats of audio / video tracks
-    'merge_output_format': 'mkv',
+    'format': 'bestvideo*+bestaudio/best',
+
+    'merge_output_format': 'mp4/mkv',
 
     # The following simply download the best available quality served as a
     # single file. Note that this may not download the best available quality
@@ -68,7 +71,7 @@ ydl_opts = {
     # quality, then convert to mp3 with a bit rate of 192 kbps.
     # Look here for a complete list of available postprocessors (remember to
     # first remove the last 'PP' from the postprocessor names):
-    # https://github.com/rg3/youtube-dl/blob/master/youtube_dl/postprocessor/__init__.py
+    # https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/postprocessor/__init__.py
     # and check out the constructor of the source code of selected
     # postprocessors for available options.
 
@@ -80,12 +83,14 @@ ydl_opts = {
     # }],
 
     # -- Set up how the downloaded files are renamed and organized --------------
-    # Ref: https://github.com/rg3/youtube-dl/blob/master/README.md#output-template
+    # Ref: https://github.com/yt-dlp/yt-dlp#output-template
 
     # Template for video playlists
+
     'outtmpl': '%(playlist)s/%(playlist_index)s- %(title)s.%(ext)s',
 
     # Template for single video urls
+
     # 'outtmpl': '%(title)s - %(uploader)s.%(ext)s',
 
     # -- Subtitle download settings ---------------------------------------
@@ -93,7 +98,7 @@ ydl_opts = {
     'writesubtitles': DOWN_SUB,
     # 'subtitleslangs': ['en'],
     'allsubtitles': True,
-    'subtitlesformat': 'srt',  # prefer subtitles in SRT format
+    'subtitlesformat': 'ass/srt/best',
 
     # ---------------------------------------------------------------------
 
@@ -101,11 +106,17 @@ ydl_opts = {
     # videos from a playlist is downloaded; comment this out if you don't need
     # this feature and don't like the log file to be generated in program's
     # root directory.
-    'download_archive': 'download_history.log'
+
+    'download_archive': 'download_history.log',
+
+    # Number of fragments of a dash/hlsnative video that should be
+    # downloaded concurrently.
+
+    'concurrent_fragment_downloads': 5
 }
 if USE_COOKIES:
-    ydl_opts['cookiefile'] = COOKIEFILE
-else:
+    ydl_opts['cookiesfrombrowser'] = COOKIES_CONFIG
+elif USE_COOKIES is not None:
     ydl_opts.update({
         'username': USERNAME,
         'password': PASSWORD
@@ -117,10 +128,10 @@ with open(URLFILE, mode='r') as f:
     urls = [line.strip() for line in f]
 
 if urls:
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    with YoutubeDL(ydl_opts) as ydl:
         # This is equivalent to the following command line executed once for
         # each URL:
-        # youtube-dl --cookies cookies.txt --all-subs --write-sub --sub-format "srt" -f "best" -o "Lynda.com- %(playlist)s/%(chapter)s/%(playlist_index)s- %(title)s.%(ext)s" "YOUR_URL"
+        # yt-dlp --cookies cookies.txt --all-subs --write-sub --sub-format "srt" -f "best" -o "Lynda.com- %(playlist)s/%(chapter)s/%(playlist_index)s- %(title)s.%(ext)s" "YOUR_URL"
         ydl.download(urls)
 else:
     print('Please check whether the location of urls.txt is correct and the file is not empty.')
